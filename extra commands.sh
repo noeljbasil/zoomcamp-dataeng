ab19f98a1065c46306c59ea5094928e30d598048
Noel Joe Basil <67711280+noeljbasil@users.noreply.github.com>
2024-04-05 06:36:23 +0000

    Add pandas to requirements.txt and update extra commands.sh

--- extra commands.sh
+++ extra commands.sh
@@ -1,13 +1,34 @@
+// Download the data
+wget https://github.com/DataTalksClub/nyc-tlc-data/releases/download/yellow/yellow_tripdata_2021-01.csv.gz
+
+// Unzip the data
+gzip -d yellow_tripdata_2021-01.csv.gz
+
+// Connect to the database
+pgcli -h localhost -p 5432 -u root -d ny_taxi
+
+// create docker network
+docker network create pg-network
+
+// Create a new database
 docker run -it \
     -e POSTGRES_USER="root" \
     -e POSTGRES_PASSWORD="root" \
     -e POSTGRES_DB="ny_taxi" \
     -v $(pwd)/ny_taxi_postgres_data:/var/lib/postgresql/data \
     -p 5432:5432 \
+    --network=pg-network \
+    --name pg-database \
     postgres:13
 
-wget https://github.com/DataTalksClub/nyc-tlc-data/releases/download/yellow/yellow_tripdata_2021-01.csv.gz
+// create pg admin server
+docker run -it \
+    -e PGADMIN_DEFAULT_EMAIL="admin@admin.com" \
+    -e PGADMIN_DEFAULT_PASSWORD="root" \
+    -p 8080:80 \
+    --network=pg-network \
+    --name pgadmin \
+    dpage/pgadmin4
 
-gzip -d yellow_tripdata_2021-01.csv.gz
-
-pgcli -h localhost -p 5432 -u root -d ny_taxi
+// convert ipynb to script
+jupyter nbconvert --to script upload_data.ipynb
