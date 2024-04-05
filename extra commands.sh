3fc319fcb8fe84b269f10e925bf472d41dc4bcb4
Noel Joe Basil <67711280+noeljbasil@users.noreply.github.com>
2024-04-05 07:44:29 +0000

    update extra commands

--- extra commands.sh
+++ extra commands.sh
@@ -32,3 +32,15 @@ docker run -it \
 
 // convert ipynb to script
 jupyter nbconvert --to script upload_data.ipynb
+
+// run the data ingestion script
+URL="https://github.com/DataTalksClub/nyc-tlc-data/releases/download/yellow/yellow_tripdata_2021-01.csv.gz"
+
+python ingest_data.py \
+    --user=root \
+    --password=root \
+    --host=localhost \
+    --port=5432 \
+    --db=ny_taxi \
+    --table-name=ny_taxi_trips \
+    --url=${URL}
