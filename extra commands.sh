d9aa4e57d5d8c9bcfa8a605340bb8dd5d5dfe2ec
Noel Joe Basil <67711280+noeljbasil@users.noreply.github.com>
2024-04-07 18:12:14 +0100

    Feature/week1 (#1)
    * add .devcontainer configs
    
    * Update devcontainer.json with new extension
    
    * update devcontainer
    
    * testing out new post create command
    
    * Add jupyter to requirements.txt
    
    * Update postCreateCommand in devcontainer.json
    
    * Update postCreateCommand in devcontainer.json
    
    * Update devcontainer.json to remove commented code
    
    * Update Makefile
    
    * Update Makefile
    
    * Update Makefile
    
    * Update requirements.txt
    
    * Add pgcli to requirements.txt
    
    * Add SQLAlchemy to requirements.txt
    
    * Add script to run PostgreSQL container
    
    * Add reference for extra commands and remove old container command
    Also update the notebook to upload data to postgres db
    
    * Add pandas to requirements.txt and update extra commands.sh
    
    * update extra commands
    
    * script to ingest data
    
    * update requirements.txt
    
    * add extra commands and create docker file for containerizing ingestion script
    
    * test verification
    
    * add docker compose and updated command reference file
    
    * formatted

---
+++ extra commands.sh
@@ -0,0 +1,63 @@
+// Download the data
+wget https://github.com/DataTalksClub/nyc-tlc-data/releases/download/yellow/yellow_tripdata_2021-01.csv.gz
+
+// Unzip the data
+gzip -d yellow_tripdata_2021-01.csv.gz
+
+// Connect to the database
+pgcli -h localhost -p 5432 -u root -d ny_taxi
+
+// create docker network
+docker network create pg-network
+
+// Create a new database in a container in a network
+docker run -it \
+    -e POSTGRES_USER="root" \
+    -e POSTGRES_PASSWORD="root" \
+    -e POSTGRES_DB="ny_taxi" \
+    -v $(pwd)/ny_taxi_postgres_data:/var/lib/postgresql/data \
+    -p 5432:5432 \
+    --network=pg-network \
+    --name pg-database \
+    postgres:13
+
+// create pg admin server in a container in a network
+docker run -it \
+    -e PGADMIN_DEFAULT_EMAIL="admin@admin.com" \
+    -e PGADMIN_DEFAULT_PASSWORD="root" \
+    -p 8080:80 \
+    --network=pg-network \
+    --name pgadmin \
+    dpage/pgadmin4
+
+// convert ipynb to script
+jupyter nbconvert --to script upload_data.ipynb
+
+// run the data ingestion script
+URL="https://github.com/DataTalksClub/nyc-tlc-data/releases/download/yellow/yellow_tripdata_2021-01.csv.gz"
+
+python ingest_data.py \
+    --user=root \
+    --password=root \
+    --host=localhost \
+    --port=5432 \
+    --db=ny_taxi \
+    --table-name=ny_taxi_trips \
+    --url=${URL}
+
+// dockerize data ingestion
+docker build -t taxi_ingest:v001 .
+
+docker run -it \
+    --network=pg-network \
+    taxi_ingest:v001 \
+        --user=root \
+        --password=root \
+        --host=pg-database \
+        --port=5432 \
+        --db=ny_taxi \
+        --table-name=ny_taxi_trips \
+        --url=${URL}
+
+// running docker compose
+docker-compose up
